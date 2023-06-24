5905948086a7d6bf468c30dcdfa2acabb970c3a7
Arthur de Souza Spironello <82726062+Deive25@users.noreply.github.com>
2023-06-24 18:55:40 -0300

    Criação das controllers de cliente e funcionario

--- app/src/main/java/com/example/academia/controller/ClienteControler.java
+++ app/src/main/java/com/example/academia/controller/ClienteControler.java
@@ -1,4 +1,54 @@
 package com.example.academia.controller;
 
+import com.example.academia.model.Cliente;
+import com.example.academia.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Controller
+@RequestMapping(path = "/cliente")
 public class ClienteControler {
+
+    @Autowired
+    private ClienteRepository dao;
+
+    @GetMapping("/cadastrar")
+    public String cadastrarCliente(Model model) {
+        model.addAttribute("cliente",new Cliente());
+        return "";
+    }
+
+    @GetMapping("/Cliente/{id}")
+    public String findCliente(Model model, @PathVariable Long id) {
+        Cliente cliente = dao.findById(Math.toIntExact(id)).get();
+
+        model.addAttribute("cliente",cliente);
+        return "";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirCliente(Model model, @PathVariable Long id) {
+        dao.deleteById(Math.toIntExact(id));
+
+        return "";
+    }
+
+    @PostMapping("/save")
+    public String salvarCliente(@ModelAttribute Cliente cliente, Model model) {
+        dao.save(cliente);
+
+        return "";
+    }
+
+    @GetMapping("/listar")
+    public String listarClientes(Model model) {
+
+        List<Cliente> listCliente = (List<Cliente>) dao.findAll();
+        model.addAttribute("clientes", listCliente);
+        return "";
+    }
+
 }
