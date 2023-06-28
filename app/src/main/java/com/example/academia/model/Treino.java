349e6e52bd09f65c7cdbff0c1d5e3dd7809c1298
Arthur de Souza Spironello <82726062+Deive25@users.noreply.github.com>
2023-06-28 16:57:50 -0300

    Criação das model e repository de Treino

---
+++ app/src/main/java/com/example/academia/model/Treino.java
@@ -0,0 +1,41 @@
+package com.example.academia.model;
+
+import jakarta.persistence.*;
+import com.example.academia.model.Exercicio;
+import java.util.Date;
+
+@Entity
+@Table(name = "treinos")
+public class Treino {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private int ordem;
+    @Column(length = 20)
+    private String series;
+    @Column
+    private int repeticao;
+    @Column
+    private int carga;
+    @Column(length = 20)
+    private String tipoTreino;
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date trocaTreino;
+    @Column
+    private String observacao;
+    @Column(columnDefinition = "BIT DEFAULT 1")
+    private boolean ativo;
+    @ManyToOne
+    @JoinColumn(name = "FK_exercicios")
+    private Exercicio exercicio;
+
+}
+
+
+
+
+
+
